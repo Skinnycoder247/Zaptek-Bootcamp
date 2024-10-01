d6e408d8271d6257d517953af6f878bc99b07e83
Skinnycoder247 <kojoe278@gmail.com>
2024-10-01 00:48:59 +0000

    new file:   Objects.js

---
+++ Objects.js
@@ -0,0 +1,9 @@
+
+const student = {};
+student.firstName ="Christiano"
+student.lastName = "Ronaldo"
+student.emailAddress = "Suiii123@gmail.com"
+student.phoneNumber = "555-6312-1447"
+student.course = "Mobile App Development"
+student.campus = "KNUST"
+
